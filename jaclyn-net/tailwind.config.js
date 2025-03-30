78cc88bb3d6a1b335f07d84376e1a40ef51df442
Jaclyn Pham <pham02@allegheny.edu>
2025-03-30 19:10:19 -0400

    universal styling setup

--- jaclyn-net/tailwind.config.js
+++ jaclyn-net/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'natural': {
+          sand: '#A99F77',
+          earth: '#605931',
+          sky: '#62A9D4',
+          forest: '#41461A',
+          mist: '#C0D0D1',
+        }
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'sf-pro': ['-apple-system', 'SF Pro Display', 'SF Pro Text', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 } 
